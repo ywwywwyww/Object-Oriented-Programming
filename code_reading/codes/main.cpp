00f2bd671b94118e8ce95f4fcb0cb2aed7153c56
ywwywwyww <weeerrr720@qq.com>
2020-05-15 18:45:25 +0800

    add code reading

---
+++ code_reading/codes/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
