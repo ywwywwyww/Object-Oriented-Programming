e2413349e841ac48bafd33ceb30e98e160ad422c
ywwywwyww <weeerrr720@qq.com>
2020-05-16 21:33:00 +0800

    update code reading / codes

--- code_reading/codes/main.cpp
+++ code_reading/codes/main.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
+/**
+ * @file main.cpp
+ * @author Wuwei YUAN
+ * @date 5/15/20
+ */
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-
+void PointsAndSegment();
+void Surprising();
+void Exact();
+void ArrayConvexHull2();
+void VectorConvexHull2();
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  PointsAndSegment();
+//  Surprising();
+//  Exact();
+//  ArrayConvexHull2();
+//  VectorConvexHull2();
   return 0;
 }
